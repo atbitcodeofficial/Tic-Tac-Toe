2489a76bb72a1920f31e4b136e4b50648154c6ef
atbitcodeofficial <157035354+atbitcodeofficial@users.noreply.github.com>
2024-01-27 21:47:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
-console.log(`welcome to tic tac toe`);
-let box = document.querySelectorAll(".box");
-let txt = document.querySelector(".txt");
-let player = "x";
-let winSound = new Audio("win1.wav");
-let tieSound = new Audio("tie.wav");
-let xSound = new Audio("x.wav");
-let oSound = new Audio("o.wav");
+  let box = document.querySelectorAll(".box");
+        let txt1 = document.getElementById("txt");
+        let screen = document.querySelector(".screen");
+        let oTurnSound = new Audio("o.mp3");
+        let xTurnSound = new Audio("x.wav");
+        let winSound = new Audio("win.mp3");
+        let tieSound = new Audio("tie.mp3");
 
-
-function changePlayer() {
-    if(player == "x"){
-        player = "o";
-    } else {
-        player = "x";
-    }
-}
-
-let possiblity = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-function checkWin() {
-    return possiblity.some(a =>{
-      return a.every(b =>{
-            return box[b].classList.contains(player);
-        })
-    })
-}
-
-function checkDraw() {
-    return [...box].every(c =>{
-        return c.classList.contains("x") || c.classList.contains("o");
-    })
-}
-
-function reloadGame() {
-    setTimeout(() => {
-        location.reload();
-    }, 2000);
-}
-
-box.forEach(element => {
-    element.addEventListener("click", ()=>{
-        element.textContent = player;
-        element.classList.add("disabled");
-        element.classList.add(player);
-        if (player == "x") {
-            xSound.play();
-        } else {
-            oSound.play();
+        let currentPlayer = "X";
+        function changePlayer() {
+            if (currentPlayer == `X`) {
+                currentPlayer = `O`;
+            } else {
+                currentPlayer = `X`;
+            }
         }
-        if (checkWin() === true) {
-            txt.textContent = `yay ${player} has won the match`;
-            document.body.style.backgroundColor = "lightgreen";
-            winSound.play()
-            reloadGame()
-        } else if (checkDraw() === true){
-            txt.textContent = `ohh its tie no one wins`
-            document.body.style.backgroundColor = "lightsalmon";
-            tieSound.play()
-            reloadGame()
-        } 
-        else {
-            changePlayer();
-            txt.textContent = player;
+
+        function playPlayerSound() {
+            if (currentPlayer == `X`) {
+                xTurnSound.play();
+            } else {
+                oTurnSound.play();
+            }
         }
-    })  
-});
+
+        function showScreen() {
+            screen.classList.add("screenShow");
+            setTimeout(() => {
+                screen.classList.remove("screenShow");
+            }, 2000);
+        }
+
+        let winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        function checkWin() {
+            return winningConditions.some(a => {
+                return a.every(b => {
+                    return box[b].classList.contains(currentPlayer);
+                })
+            })
+        }
+
+        function checkDraw() {
+            return [...box].every(c => {
+                return c.classList.contains("X") || c.classList.contains("O");
+            })
+        }
+
+        function reloadPage() {
+            box.forEach(cell => {
+                setTimeout(() => {
+                    cell.textContent = "";
+                    cell.classList.remove("disabled");
+                    cell.classList.remove("X");
+                    cell.classList.remove("O");
+                    txt.textContent = `tIC tAC tOE`;
+                    changePlayer();
+                }, 2000);
+            })
+        }
+
+        box.forEach(cell => {
+            cell.addEventListener("click", () => {
+                cell.textContent = currentPlayer;
+                playPlayerSound();
+                cell.classList.add(currentPlayer);
+                if (checkWin()) {
+                    txt.textContent = currentPlayer + ` WINS`;
+                    winSound.play();
+                    reloadPage();
+                    showScreen();
+                } else if (checkDraw()) {
+                    txt.textContent = `IT'S DRAW`;
+                    tieSound.play();
+                    reloadPage();
+                    showScreen();
+                } else {
+                    changePlayer();
+                }
+                cell.classList.add("disabled");
+            })
+        });
