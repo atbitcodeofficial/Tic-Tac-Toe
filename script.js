5a39f9752ace9a551843d17bf16ec663969b94b7
atbitcodeofficial <157035354+atbitcodeofficial@users.noreply.github.com>
2024-01-28 19:30:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-  let box = document.querySelectorAll(".box");
+ let box = document.querySelectorAll(".box");
         let txt1 = document.getElementById("txt");
         let screen = document.querySelector(".screen");
+        let subTxt = document.getElementById("subTxt");
         let oTurnSound = new Audio("o.mp3");
         let xTurnSound = new Audio("x.wav");
         let winSound = new Audio("win.mp3");
         let tieSound = new Audio("tie.mp3");
+        let xScoreTxt = document.querySelectorAll(".xScore");
+        let oScoreTxt = document.querySelectorAll(".oScore");
+        let leadTxt = document.querySelectorAll(".lead");
+
 
         let currentPlayer = "X";
+        setTimeout(() => {
+            subTxt.textContent = `Turn for ` + currentPlayer;
+        }, 500);
         function changePlayer() {
             if (currentPlayer == `X`) {
                 currentPlayer = `O`;
@@ -31,7 +39,7 @@
         }
 
         let winningConditions = [
-            [0, 1, 2],
+            [0, 1, 2,],
             [3, 4, 5],
             [6, 7, 8],
             [0, 3, 6],
@@ -49,9 +57,64 @@
             })
         }
 
+        function colorBox() {
+            winningConditions.some(a => {
+                a.every(b => {
+                    document.querySelectorAll("." + currentPlayer).forEach(box => {
+                        box.style.backgroundColor = "lightgreen";
+                        setTimeout(() => {
+                            box.style.backgroundColor = "white";
+                        }, 2000);
+                    })
+                })
+            })
+        }
+
+        checkWin()
+
         function checkDraw() {
             return [...box].every(c => {
                 return c.classList.contains("X") || c.classList.contains("O");
+            })
+        }
+
+        let xWinCount = 0;
+        let oWinCount = 0;
+
+        function updateScore() {
+            if (currentPlayer == `X`) {
+                xWinCount = xWinCount + 1;
+                xScoreTxt.forEach(txt => {
+                    txt.textContent = `X: ` + xWinCount;
+                })
+            } else {
+                oWinCount = oWinCount + 1;
+                oScoreTxt.forEach(txt => {
+                    txt.textContent = `O: ` + oWinCount;
+                })
+            }
+            leadTxt.forEach(txt => {
+                if (xWinCount > oWinCount) {
+                    txt.textContent = `X Has Higest Score`;
+                } else if (xWinCount == oWinCount) {
+                    txt.textContent = `Both Have Equal Score`;
+                }
+                else {
+                    txt.textContent = `O Has Higest Score`;
+                }
+            })
+        }
+
+        function tieUpdateScore() { 
+            leadTxt.forEach(txt => {
+                if (xWinCount > oWinCount) {
+                    txt.textContent = `That Was Tie X Has Higest Score`;
+                } else if (xWinCount == oWinCount) {
+                    txt.textContent = `That Was Tie Both Have Equal Score`;
+                }
+                else {
+                    txt.textContent = `That Was Tie O Has Higest Score`;
+                }
             })
         }
 
@@ -64,6 +127,7 @@
                     cell.classList.remove("O");
                     txt.textContent = `tIC tAC tOE`;
                     changePlayer();
+                    subTxt.textContent = `Turn for ` + currentPlayer;
                 }, 2000);
             })
         }
@@ -78,13 +142,17 @@
                     winSound.play();
                     reloadPage();
                     showScreen();
+                    colorBox();
+                    updateScore();
                 } else if (checkDraw()) {
                     txt.textContent = `IT'S DRAW`;
                     tieSound.play();
                     reloadPage();
                     showScreen();
+                    tieUpdateScore();
                 } else {
                     changePlayer();
+                    subTxt.textContent = `Turn For ` + currentPlayer;
                 }
                 cell.classList.add("disabled");
             })
